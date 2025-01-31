9ce4d64859dd17af979e60672f89f5fec6f79520
abhijeetkumarshukla <shuklaabhijeet510@gmail.com>
2025-01-31 23:45:02 +0530

    cors error

--- backend/index.js
+++ backend/index.js
@@ -10,9 +10,10 @@ const cors = require('cors')
 const port = process.env.PORT 
 
 app.use(express.json())
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use('/user', userRouter)
 app.use('/product',authUser,productRouter)
-app.use(cors({  origin:"*"}))
+ 
 app.get('/' , (req,res)=>{
     res.status(201).json({message:'hello mern'})
 })
